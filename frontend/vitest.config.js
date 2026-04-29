565ae18097c8e3f162729e00bf26f224ae66f75a
Davide Cavestro <davide.cavestro@gmail.com>
2026-04-29 19:14:36 +0200

    fix: frontend coverage is wrong

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      all: true,
       exclude: [
         'node_modules/',
         'dist/',
